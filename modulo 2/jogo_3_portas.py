53a1955de915430b0a9204f76f77ceb3eb436db6
ML17-dev <mgioseffileal@icloud.com>
2026-02-20 00:17:21 -0300

    Create jogo_3_portas.py

---
+++ modulo 2/jogo_3_portas.py
@@ -0,0 +1,7 @@
+q1 = input('Qual Porta você quer escolher: 1, 2 ou 3?')
+if q1 == '1':
+    print('Cuidado, um dragão de cospe fogo está saindo pela porta e irá te seguir!')
+elif q1 == "2":
+    print("Cobras! São MUITAS cobras que estão saindo pela porta! Fuja o mais rápido que puder!")
+elif q1 == "3":
+    print("O que é isso? Um portal para outra dimensão? É melhor fechar essa porta antes que Coisas Estranhas comecem a aparecer!")
